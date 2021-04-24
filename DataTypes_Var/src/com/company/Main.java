62c953d2944f9cdd94efd03f8111b2f6728ce89a
Akshita Sharma <akshita2k@gmail.com>
2021-04-24 11:26:54 +0530

    Add files via upload

---
+++ DataTypes_Var/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a1; // valid
+        int abc; // valid
+        int a_$10; // valid
+
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum); // returns the sum of the two numbers
+    }
+}
