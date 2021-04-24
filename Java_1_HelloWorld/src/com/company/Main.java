a154c67c2c4386d8bc4c99ff9d007e0988a8cf18
Akshita Sharma <akshita2k@gmail.com>
2021-04-24 10:16:10 +0530

    Add files via upload

---
+++ Java_1_HelloWorld/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company; //A package in Java is used to group related classes.
+
+public class Main {
+//main is the class name.
+    public static void main(String[] args) {
+        //main acts as an entry point to the app
+        System.out.println("Hello World"); // prints and takes the cursor to a new line
+        System.out.print("Hello World "); // prints and keeps the cursor on the sma eline
+        System.out.printf("Hello World"); // andantes keeps the cursor on the new line using C format
+    }
+}
