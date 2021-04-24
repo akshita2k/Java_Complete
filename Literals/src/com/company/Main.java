250ce265c4bd67166e5b5bf29ec60ccea38a820e
Akshita Sharma <akshita2k@gmail.com>
2021-04-24 11:43:46 +0530

    Add files via upload

---
+++ Literals/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        byte age = 34;
+        char a = 'A';
+        float f1 = 5.6f;
+        double d1 = 5.6;
+        short age3 = 87;
+        String str = "Hello";
+        System.out.println(str);
+        System.out.println("Age:" + age );
+        System.out.println(a);
+        System.out.println(f1);
+        System.out.println(d1);
+
+    }
+}
