ee5669325a910a35cfef9278c078a6e0650309f6
Akshita <akshita2k@gmail.com>
2021-05-10 18:49:20 +0530

    Array-maxElement

--- Array/src/com/company/maxEle.java
+++ Array/src/com/company/maxEle.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class maxEle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int max =0;
         System.out.println("Enter the size of the array");
         int n= sc.nextInt();
         int [] a = new int [100];
@@ -11,5 +12,16 @@ public class maxEle {
         for (int i=0; i<n; i++) {
             a[i] = sc.nextInt();
         }
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (a[i]>a[j]) {
+                    max = a[i];
+                }
+                else {
+                    max = a[j];
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
