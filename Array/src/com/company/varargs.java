4633f6450bd0e5b900e670e61e061eef44d11a72
akshita2k <akshita2k@gmail.com>
2021-05-22 21:24:36 +0530

    Method Overloading - varArgs

--- Array/src/com/company/varargs.java
+++ Array/src/com/company/varargs.java
@@ -1,15 +1,22 @@
 package com.company;
 
 public class varargs {
-    static int sum(int a, int b) {
+    /*static int sum(int a, int b) {
         return (a+b);
     }
     static int sum(int a, int b, int c) {
         return (a+b+c);
+    }*/
+    static int sum(int ...arr) {
+        int res =0;
+        for (int a: arr) {
+            res += a;
+        }
+        return res;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to varAgrs tutorial");
-        System.out.println(sum(4,5));
+        //System.out.println(sum(4,5));
         System.out.println(sum(4,5,6));
     }
 }
