fd8cd6723b5d7a90a59c02960feb6c3356bfbdd4
akshita2k <akshita2k@gmail.com>
2021-05-25 12:11:21 +0530

    How to take inputs

--- Inputs/src/com/company/Main.java
+++ Inputs/src/com/company/Main.java
@@ -6,15 +6,25 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner sc = new Scanner (System.in);
+        // Scans the input entered by the user
         System.out.println("Enter your usn");
+        // Enter your usn
         int usn = sc.nextInt();
+        // takes the input
+
         // System.out.println(sc.hasNextInt(usn));
+        // checks if the usn entered is an int or not
+
         System.out.println("Enter your first name");
         String fname = sc.next();
+
         System.out.println("Enter your last name");
         String lname = sc.next();
+
         System.out.println("Enter your email");
         String email = sc.next();
+
+        //prints the details entered
         System.out.println("Your details are:");
         System.out.println("USN : " + usn);
         System.out.println("First Name : " + fname);
