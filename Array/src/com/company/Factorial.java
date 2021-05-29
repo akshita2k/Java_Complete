6f4ecbd87e29959a5888ef11145c922d06da3227
akshita2k <akshita2k@gmail.com>
2021-05-29 09:57:52 +0530

    Factorial of n

--- Array/src/com/company/Factorial.java
+++ Array/src/com/company/Factorial.java
@@ -4,6 +4,15 @@ import java.util.*;
 public class Factorial {
     static int  factorial(int n) {
         int fact;
+        // can be done in two ways
+        // 1. For loop
+        // 2. Recursion
+        /* fact = 1;
+        for (int i=1; i<=n; i++) {
+            fact= fact*n;
+        }
+        return fact */
+
         if (n==0 || n==1) {
             return 1;
         }
