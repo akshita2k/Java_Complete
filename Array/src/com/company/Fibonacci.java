cc19b1da58f34c3bf5f8f6db665d6b8f79d66bce
akshita2k <akshita2k@gmail.com>
2021-05-29 10:15:17 +0530

    Fibonacci Series of n

---
+++ Array/src/com/company/Fibonacci.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class Fibonacci {
+    static int fibonacci(int n) {
+        if (n==0) {
+            return 0;
+        }
+        else if (n==1) {
+            return 1;
+        }
+        else {
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+}
