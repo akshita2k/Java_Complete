b22cd3a8c3694b60f1c2ae09e153fb18e5c19681
akshita2k <akshita2k@gmail.com>
2021-05-29 10:35:24 +0530

    Reverse the Pattern

---
+++ Array/src/com/company/RevPattern.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class RevPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for (int i=n; i>=1; i--) {
+            for (int j=i; j>=1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
