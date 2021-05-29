b5b2fc04d31dbb6c5cb31c4d9222b834ca4a1b13
akshita2k <akshita2k@gmail.com>
2021-05-29 10:21:10 +0530

    Multiplication Table

---
+++ Array/src/com/company/MulTable.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.*;
+
+public class MulTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i=1; i<11; i++) {
+            System.out.println( num +" * "+ i +" = "+ (num*i) );
+        }
+    }
+}
