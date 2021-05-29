cb14af909d2353d31cb2c4f5f0adcfe70850fa60
akshita2k <akshita2k@gmail.com>
2021-05-29 16:51:48 +0530

    Pattern

--- Array/src/com/company/Pattern.java
+++ Array/src/com/company/Pattern.java
@@ -2,14 +2,23 @@ package com.company;
 import java.util.*;
 
 public class Pattern {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int n = sc.nextInt();
+    static void pattern(int n) {
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=i; j++) {
                 System.out.print("* ");
             }
             System.out.println("\n");
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        pattern(n);
+        /* for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        } */
     }
 }
