3f18353ddb2f4a4b87df3bb6d25d1ef490994b0c
akshita2k <akshita2k@gmail.com>
2021-05-31 23:22:26 +0530

    Day2-Challenge

---
+++ Daily Code Interview Solutions/src/DailyCodeInterview/Day2.java
@@ -0,0 +1,32 @@
+package DailyCodeInterview;
+import java.util.*;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the size");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements of the array");
+        int arr[] = new int [n];
+        for (int i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+
+
+
+        int res[] = new int [n];
+
+        for (int i=0; i<n; i++) {
+            int prod =1;
+            for (int j=0; j<n; j++) {
+                while (j!=i) {
+                    prod = arr[j] * prod;
+                }
+            }
+            res[i] = prod;
+        }
+
+        for (int i=0; i<n; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
