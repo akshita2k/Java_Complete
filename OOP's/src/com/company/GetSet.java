07fd08f60af7e3b4b1510ece82fe92999a9fe637
akshita2k <akshita2k@gmail.com>
2021-06-01 22:05:04 +0530

    Getter&Setters

---
+++ OOP's/src/com/company/GetSet.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.*;
+
+class emp3 {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String n) {
+        name = n;
+    }
+    public void setId(int i) {
+        id = i;
+    }
+    public int getId() {
+        return id;
+    }
+  /*  public emp3() {
+        id = 31;
+        name = "Your name";
+    } */
+    public emp3 (String myname, int n) {
+        id= n;
+        name = myname;
+    }
+}
+
+public class GetSet {
+    public static void main(String[] args) {
+        emp3 e1 = new emp3("James", 31);
+        // e1.setName("James");
+        // e1.setId(31);
+        System.out.println(e1.getId());
+        System.out.println(e1.getName());
+    }
+}
